575fe216b311ac564837f211a850724ec0403e0c
watanta <watanabe101watanabe@yahoo.co.jp>
2018-07-11 21:51:57 +0900

    全部のボドゲのページをスクレイピングできるようにした

--- bdgmscrapy/bdgmscrapy/spiders/games.py
+++ bdgmscrapy/bdgmscrapy/spiders/games.py
@@ -16,7 +16,19 @@ class GamesSpider(scrapy.Spider):
     	for url in urls:
     		absolute_url = response.urljoin(url)
     		logger.info(absolute_url)
-    		yield Request(absolute_url, callback=self.parse_book)
+    		yield Request(absolute_url, callback=self.parse_game)
 
-    def parse_book(self, response):
-    	pass
+    	next_page_url = response.xpath('//*[@id="main-content"]//a[@rel="next"]/@href').extract_first()
+    	absolute_next_page_url = response.urljoin(next_page_url)
+    	yield Request(absolute_next_page_url)
+
+
+    def parse_game(self, response):
+
+    	title = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[0]
+    	en_title = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[1]
+    	players = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[2]
+    	target_age = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[3]
+    	published_at = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[4]
+    	price = response.xpath('//*[@id="games-detail"]/div/div[4]/table//td/text()').extract()[5]
+    	yield {'title':title, 'en_title':en_title, 'players':players, 'target_age':target_age, 'published_at':published_at, 'price':price}
